6ea8d52195461352442360e6417970a69b83185f
Cengizhan55 <cengiz_er_@hotmail.com>
2025-05-20 10:14:18 +0300

    refactor

--- main/main.go
+++ main/main.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-        "encoding/json"
-        "fmt"
-        "log"
-        "net/http"
-        )
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
 
-// Kullanıcı modelimiz
-        type User struct {
-        Name  string `json:"name"`
-        Email string `json:"email"`
-        }
+// User Model
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
 
-// Ana sayfa handler
-        func homeHandler(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintln(w, "Merhaba, Go Web Sunucusuna Hoş Geldiniz!")
-        }
+// Main Page Handler
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello! Welcome to Go Server")
+}
 
-// JSON verisi alan handler
-        func userHandler(w http.ResponseWriter, r *http.Request) {
-        if r.Method != http.MethodPost {
-        http.Error(w, "Sadece POST metodu destekleniyor.", http.StatusMethodNotAllowed)
-        return
-        }
+// Json Data Handler
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only Post Method supported !", http.StatusMethodNotAllowed)
+		return
+	}
 
-        var user User
-        err := json.NewDecoder(r.Body).Decode(&user)
-        if err != nil {
-        http.Error(w, "Geçersiz JSON", http.StatusBadRequest)
-        return
-        }
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Unvalid JSON", http.StatusBadRequest)
+		return
+	}
 
-        response := fmt.Sprintf("Kullanıcı alındı: %s <%s>", user.Name, user.Email)
-        fmt.Fprintln(w, response)
-        }
+	response := fmt.Sprintf("User received: %s <%s>", user.Name, user.Email)
+	fmt.Fprintln(w, response)
+}
 
-        func main() {
-        http.HandleFunc("/", homeHandler)
-        http.HandleFunc("/user", userHandler)
+func main() {
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/user", userHandler)
 
-        fmt.Println("Sunucu 8080 portunda çalışıyor...")
-        log.Fatal(http.ListenAndServe(":8080", nil))
-        }
+	fmt.Println("Server running on port 8080 ..")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
